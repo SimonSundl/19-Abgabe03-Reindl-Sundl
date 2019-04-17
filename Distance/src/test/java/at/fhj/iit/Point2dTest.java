b05ddc22246afe5293589bef3d3e70fac63f810c
Simon Sundl <simon.sundl@edu.fh-joanneum.at>
2019-04-17 14:56:26 +0200

    Added distance program and implemented first testcase

---
+++ Distance/src/test/java/at/fhj/iit/Point2dTest.java
@@ -0,0 +1,67 @@
+package at.fhj.iit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Point2dTest {
+
+	/**
+	 * Set some Point2d for testing their functionality
+	 */
+
+	private Point2d p2D;
+
+	@Before
+	public void setup(){
+
+		// SETUP PHASE
+		p2D = new Point2d(0, 0);
+	}
+
+	/**
+	 * Test the Setter of the x-coordinate
+	 * - set the coordinate
+	 * - get the value from the object
+	 * - try to compare your value with the return value of x
+	 */
+	@Test
+	public void testSetX(){
+		p2D.setX(5);
+
+		assertEquals(5, p2D.getX(), 0.001);
+	}
+
+	/**
+	 * Test the Setter of the y-coordinate
+	 * - set the coordinate
+	 * - get the value from the object
+	 * - try to compare your value with the return value of y
+	 */
+	@Test
+	public void testSetY(){
+	}
+
+
+	/**
+	 * Test the distanceFrom() Method
+	 * - call the method distanceFrom and calculate the distance
+	 * - calculate the distance by your own implementation
+	 * - try to compare your result with the return value of distanceFrom()
+	 */
+	@Test
+	public void testDistanceFrom(){
+	}
+
+	/**
+	 * Test the distanceFromOrigin() Method
+	 * - call the method distanceFromOrigin and calculate the distance
+	 * - calculate the distance by your own implementation
+	 * - try to compare your result with the return value of distanceFromOrigin()
+	 */
+	@Test
+	public void testDistanceFromOrigin(){
+	}
+}
